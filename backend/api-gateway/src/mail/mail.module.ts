0f4fb6141d05e14022e56ef82df4ef843611aff3
Çağatay <durucagatay01@gmail.com>
2023-10-09 13:45:04 +0300

    Dockerized everything

--- backend/api-gateway/src/mail/mail.module.ts
+++ backend/api-gateway/src/mail/mail.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.TCP,
+        options: {
+          host: process.env.USER_SERVICE_HOST || 'localhost',
+          port: 4002,
+        },  
+      },
+    ]),
+  ],
   controllers: [MailController]
 })
 export class MailModule {}
