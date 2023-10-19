eb93602501d5466cfea6caee0cf8245a141ffc51
Çağatay <durucagatay01@gmail.com>
2023-10-19 15:56:55 +0300

    src bulk commit

--- backend/charge-point-node/src/app.controller.ts
+++ backend/charge-point-node/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { RabbitmqService } from './ocpp/rabbitmq.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    private readonly rabbitMQService: RabbitmqService,
+  ) {}
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    this.rabbitMQService.send('rabbit-mq-producer', {
+      message: this.appService.getHello(),
+    });
+    return 'Message sent to the queue!';
   }
 }
--- backend/charge-point-node/src/app.module.ts
+++ backend/charge-point-node/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OcppModule } from './ocpp/ocpp.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [OcppModule,
+    ClientsModule.register([
+    {
+      name: 'rabbit-mq-module',
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqp://csms:csms@localhost:5672/',
+        ],
+        queue: 'rabbit-mq-nest-js',
+      },
+    },
+  ]),
+],
   controllers: [AppController],
   providers: [AppService],
 })
